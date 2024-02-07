6e97b1961edb44f5e9a910b209433d9e43741462
Nicolas14200 <heutte.nicolas@laposte.net>
2024-02-07 18:05:25 +0100

    fix buzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,11 +1,11 @@
-function fizzBuzz(n) {
-    for (let i = 1; i <= n; i++) {
-      let output = "";
-      if (i % 3 === 0) output += "Fizz";
-      if (i % 5 === 0) output += "Buzz";
-      console.log(output || i);
-    }
+const fizzBuzz = (n) => {
+  for (let i = 1; i <= n; i++) {
+    let output = "";
+    if (i % 3 === 0) output += "Fizz";
+    if (i % 5 === 0) output += "Buzz";
+    console.log(output || i);
   }
-  
-  const n = process.argv[2];
-  fizzBuzz(parseInt(n));
+};
+
+const n = process.argv[2];
+fizzBuzz(n);
